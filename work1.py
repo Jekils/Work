95586fd7b32dae1fd6785225f0715a80a8e44c22
Jekils <jekvada2002@gmail.com>
2022-07-27 16:28:15 +0300

    last com

--- work1.py
+++ work1.py
@@ -1,2 +1,9 @@
-print ("Hello work1")
-print ("2 hello")
+def is_year_leap (years) :
+    if (years % 4) == 0 :
+        return True
+    else :
+        return False
+
+print ("Enter years")
+years = int(input ())
+print (is_year_leap(years))
